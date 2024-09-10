1198d566a67b09d2eb5aa4d3e3d5102e3ded0723
Rob Caelers <rob.caelers@gmail.com>
2024-09-10 14:38:51 +0200

    Fix Postgresl namespace access

--- dev/initdb/guardrail.sql
+++ dev/initdb/guardrail.sql
@@ -1,4 +1,5 @@
 CREATE USER guardrail WITH PASSWORD 'wezei4Joozooz8To';
 CREATE DATABASE guardrail;
 GRANT ALL PRIVILEGES ON DATABASE guardrail TO guardrail;
+\c guardrail;
 GRANT ALL ON schema public TO guardrail;
